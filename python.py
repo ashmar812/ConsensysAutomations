91fa289e1741b7803590b66e601beac0bf3d0ba7
mohamad ashmar <ashmar812@gmail.com>
2022-09-14 12:47:16 +0300

    Update python.py

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # This is our shell command, executed by Popen.
-
+ssh -o StrictHostKeyChecking=no stark@52.191.129.181 
 p = subprocess.Popen("ls -lh", stdout=subprocess.PIPE, shell=True)
 
 print(p.communicate())
