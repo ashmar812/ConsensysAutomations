3603e3befa1ca1f85ba70995984d6d5dd65061ae
mohamad ashmar <ashmar812@gmail.com>
2022-09-14 12:51:08 +0300

    Update python.py

--- python.py
+++ python.py
@@ -1,2 +1,2 @@
 import subprocess
-subprocess.Popen(f"ssh {stark}@{52.191.129.181} {cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+subprocess.Popen(f"ssh {"stark"}@{"52.191.129.181"} {cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
